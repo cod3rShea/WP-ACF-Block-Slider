9814ef0e1129df713a030d7c485fa9096f4f73b2
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2020-07-04 11:29:44 -0400

    load css and js through extension

--- functions.php
+++ functions.php
@@ -19,4 +19,24 @@ include( plugin_dir_path( __FILE__ ) . 'acf/acf-fields.php');
 //creates the block template
 include( plugin_dir_path( __FILE__ ) . 'block/block-template.php');
 
- ?>
+
+// register jquery and style on initialization
+add_action('init', 'register_script');
+function register_script() {
+    wp_register_script( 'owl_slider_script_min', plugins_url('/js/owl.carousel.min.js', __FILE__), array('jquery'), '2.5.1' );
+
+    // wp_register_style( 'owl_slider_style_min', plugins_url('/css/owl.carousel.min.css', __FILE__), false, '1.0.0', 'all');
+    wp_register_style( 'owl_slider_style_custom', plugins_url('/css/style.css', __FILE__), false, '1.0.0', 'all');
+}
+
+// use the registered jquery and style above
+add_action('wp_enqueue_scripts', 'enqueue_style', 20, 1);
+
+function enqueue_style(){
+   wp_enqueue_script('owl_slider_script_min');
+
+   // wp_enqueue_style( 'owl_slider_style_min' );
+   wp_enqueue_style( 'owl_slider_style_custom' );
+}
+
+?>
